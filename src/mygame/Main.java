06b6213479d270652a4eab1e2debe7a1abd88b2d
como-quesito <celiesita1951>
2016-07-28 11:51:36 -0500

    Nuevo

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -6,6 +6,7 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -21,14 +22,10 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+       Spatial escena1=    assetManager.loadModel("Scenes/PrimeraEscena.j3o");
+       Vector3f pos=new Vector3f(0,20,-40);
+       cam.setLocation(pos);
+       
     }
 
     @Override
